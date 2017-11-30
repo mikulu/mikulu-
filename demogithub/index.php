070a1fd4a3acc4d1f020d3bcd9ff3f46c22704ae
2007maikulu <2007maikulu@PC9778.domepfc.net>
2017-11-30 21:10:43 +0100

    setup du lien Netbeans projet demogithub

---
+++ demogithub/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<!--
+Initialisation repository github
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Git check</title>
+    </head>
+    <body>
+        <?php
+        echo "hello git" ;
+        ?>
+    </body>
+</html>
